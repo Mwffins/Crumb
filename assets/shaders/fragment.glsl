bf34f3a3cac7c231de0ac7da62eed4f9c0468d0c
Moof <111189250+Mwffins@users.noreply.github.com>
2025-09-13 03:31:10 +0100

    feat: shader implementation

---
+++ assets/shaders/fragment.glsl
@@ -0,0 +1,10 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+uniform sampler2D ourTexture;
+uniform vec4 spriteColor;
+
+void main() {
+    FragColor = texture(ourTexture, TexCoord) * spriteColor;
+}
---
+++ src/Rendering/Shader.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <glad/glad.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class Shader {
+private:
+    unsigned int programID;
+    
+    std::string ReadFile(const std::string& filepath);
+    
+    void CheckCompileErrors(unsigned int shader, const std::string& type);
+    
+public:
+    Shader();
+    ~Shader();
+    
+    bool LoadFromFiles(const std::string& vertexPath, const std::string& fragmentPath);
+    
+    void Use();
+    
+    void SetBool(const std::string& name, bool value);
+    void SetInt(const std::string& name, int value);
+    void SetFloat(const std::string& name, float value);
+    void SetVec2(const std::string& name, float x, float y);
+    void SetVec3(const std::string& name, float x, float y, float z);
+    void SetVec4(const std::string& name, float x, float y, float z, float w);
+    
+    unsigned int GetProgramID() const { return programID; }
+};
