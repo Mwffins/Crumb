71ce342c84e2e5821ef7f3468da8e4de16bf630d
Moof <111189250+Mwffins@users.noreply.github.com>
2025-09-14 14:51:55 +0100

    fix: use nearest neighbour filtering (for pixel art)

--- src/Core/AssetManager.cpp
+++ src/Core/AssetManager.cpp
@@ -35,8 +35,11 @@ namespace Crumb
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
                      channels == 4 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, data);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
         stbi_image_free(data);
 
